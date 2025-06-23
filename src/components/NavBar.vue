d1e47e3bfac21b6c7510e960f21189b7a544d107
Telvin696 <mwangitelvin8@gmail.com>
2025-06-23 10:19:44 +0300

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+<v-app-bar color ="orange">
+    <v-app-bar-title>Jitegemee University</v-app-bar-title>
+    <v-btn to="/"color="black" > Home </v-btn>
+    <v-btn to="/About Us" color="black" > About us </v-btn>
+    <v-btn to="/Courses"  color="blacke" > Courses </v-btn>
+    <v-btn to="/Apply"    color="black" > Apply </v-btn>
+    <v-btn to="/Events"  color="black" > Events </v-btn>
+    <v-btn to="/Student life"color="black" > Student life </v-btn>
+    <v-btn to="/Library"  color="black" > Library </v-btn>
+    <v-btn to="/Contact us" color="black" > Contact Us </v-btn>
+    <v-btn to="/Login" color="black" > Login </v-btn>
+</v-app-bar>
+
+</template>
