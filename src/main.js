81ad428a94c3b24ae918af95212732e5b54ff453
Telvin696 <mwangitelvin8@gmail.com>
2025-06-24 12:13:20 +0300

    adding stores

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from'./router'
+import { createPinia } from 'pinia'
 
 // Vuetify
 import 'vuetify/styles'
@@ -17,10 +18,12 @@ const vuetify = createVuetify({
     defaultSet: 'mdi', // This is already the default value - only for display purposes
   },
 })
+const pinia = createPinia()
 const app =createApp(App)
 
 // app.use(createPinia())
 app.use(router)
 app.use(vuetify)
+app.use(pinia)
 
 app.mount('#app')
